d2503d51af92c9f7119d318d5343e5ea49b01b13
Peter Schuller <peter.schuller@infidyne.com>
2017-02-02 21:44:10 -0800

    Add backwards compatibility test.

--- saltybox_test.go
+++ saltybox_test.go
@@ -63,3 +63,35 @@ func TestEncryptDecrypt(t *testing.T) {
 		t.Fatal("plain text does not match original plain text")
 	}
 }
+
+func TestBackwardsCompatibility(t *testing.T) {
+	tempdir, err := ioutil.TempDir(os.TempDir(), "saltyboxtest")
+	if err != nil {
+		t.Fatalf("failed creating temp dir: %s", err)
+	}
+	defer checkedRemove(tempdir)
+
+	encryptedPath := filepath.Join(tempdir, "plain")
+	err = ioutil.WriteFile(encryptedPath, []byte("saltybox1:RF0qX8mpCMXVBq6zxHfamdiT64s6Pwvb99Qj9gV61sMAAAAAAAAAFE6RVTWMhBCMJGL0MmgdDUBHoJaW"), 0777)
+	if err != nil {
+		t.Fatalf("failed to write to %s: %s", encryptedPath, err)
+	}
+	defer checkedRemove(encryptedPath)
+
+	newPlainPath := filepath.Join(tempdir, "newplain")
+	defer checkedRemove(newPlainPath)
+
+	err = passphraseDecryptFile(encryptedPath, newPlainPath, constantPassphraseReader{constantPassphrase: "test"})
+	if err != nil {
+		t.Fatalf("decryption failed: %s", err)
+	}
+
+	newPlainText, err := ioutil.ReadFile(newPlainPath)
+	if err != nil {
+		t.Fatalf("failed to read from %s: %s", newPlainPath, err)
+	}
+
+	if !bytes.Equal(newPlainText, []byte("test")) {
+		t.Fatal("plain text does not match original plain text")
+	}
+}
