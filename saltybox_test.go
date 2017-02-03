95a2ab3a98b7acb1a5ceb9f788760e6d3ec6cba8
Peter Schuller <peter.schuller@infidyne.com>
2017-02-02 21:12:58 -0800

    Panic on error in defers in tests.

--- saltybox_test.go
+++ saltybox_test.go
@@ -8,29 +8,35 @@ import (
 	"testing"
 )
 
+func panicOnError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func TestEncryptDecrypt(t *testing.T) {
 	tempdir, err := ioutil.TempDir(os.TempDir(), "saltyboxtest")
 	if err != nil {
 		t.Fatalf("failed creating temp dir: %s", err)
 	}
-	defer os.Remove(tempdir)
+	defer panicOnError(os.Remove(tempdir))
 
 	plainPath := filepath.Join(tempdir, "plain")
 	err = ioutil.WriteFile(plainPath, []byte("super secret"), 0777)
 	if err != nil {
 		t.Fatalf("failed to write to %s: %s", plainPath, err)
 	}
-	defer os.Remove(plainPath)
+	defer panicOnError(os.Remove(plainPath))
 
 	encryptedPath := filepath.Join(tempdir, "encrypted")
-	defer os.Remove(encryptedPath)
+	defer panicOnError(os.Remove(encryptedPath))
 
-	passphraseEncryptFile(plainPath, encryptedPath, constantPassphraseReader{constantPassphrase: "test"})
+	panicOnError(passphraseEncryptFile(plainPath, encryptedPath, constantPassphraseReader{constantPassphrase: "test"}))
 
 	newPlainPath := filepath.Join(tempdir, "newplain")
-	defer os.Remove(newPlainPath)
+	defer panicOnError(os.Remove(newPlainPath))
 
-	passphraseDecryptFile(encryptedPath, newPlainPath, constantPassphraseReader{constantPassphrase: "test"})
+	panicOnError(passphraseDecryptFile(encryptedPath, newPlainPath, constantPassphraseReader{constantPassphrase: "test"}))
 
 	newPlainText, err := ioutil.ReadFile(newPlainPath)
 	if err != nil {
