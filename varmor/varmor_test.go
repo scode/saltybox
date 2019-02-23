b194d325cf07f3465a31949b62933cab2e90123b
Peter Schuller <peter.schuller@infidyne.com>
2019-02-22 21:27:50 -0800

    Improve varmor tests. (#26)

--- varmor/varmor_test.go
+++ varmor/varmor_test.go
@@ -1,20 +1,34 @@
 package varmor
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func preserve(t *testing.T, s string) {
+func preserveString(t *testing.T, s string) {
 	b, err := Unwrap(Wrap([]byte(s)))
 	assert.NoError(t, err)
 	assert.Equal(t, s, string(b))
 }
 
+func preserveBytes(t *testing.T, b []byte) {
+	wrapped, err := Unwrap(Wrap(b))
+	assert.NoError(t, err)
+	assert.EqualValues(t, b, wrapped)
+}
+
 func TestPreservation(t *testing.T) {
-	preserve(t, "")
-	preserve(t, "test")
+	preserveString(t, "")
+	preserveString(t, "test")
+
+	rnd := rand.New(rand.NewSource(0))
+	rbytes := make([]byte, 100000)
+	n, err := rnd.Read(rbytes)
+	assert.NoError(t, err)
+	assert.Equal(t, 100000, n)
+	preserveBytes(t, rbytes)
 }
 
 func TestTruncated(t *testing.T) {
@@ -28,4 +42,18 @@ func TestWrongVersion(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "input claims to be saltybox, but not a version we support", err.Error())
 	assert.Nil(t, b)
+}
+
+func TestAllByteValues(t *testing.T) {
+	allBytes := make([]byte, 256)
+	for i := 0; i <= 255; i++ {
+		allBytes[i] = byte(i)
+	}
+
+	preserveBytes(t, allBytes)
+
+	wrapped := Wrap(allBytes)
+	assert.Equal(t,
+		"saltybox1:AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0-P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn-AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq-wsbKztLW2t7i5uru8vb6_wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t_g4eLj5OXm5-jp6uvs7e7v8PHy8_T19vf4-fr7_P3-_w",
+		wrapped)
 }
