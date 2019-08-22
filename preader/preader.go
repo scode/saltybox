86aeba8f509851d980c4637a0e918cd5637e0401
Peter Schuller <peter.schuller@infidyne.com>
2019-08-21 22:18:57 -0700

    Implement passphrase reader for io.Reader, rename stdin passphrase reader.

--- preader/preader.go
+++ preader/preader.go
@@ -3,6 +3,7 @@ package preader
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -13,27 +14,21 @@ type PassphraseReader interface {
 	ReadPassphrase() (string, error)
 }
 
-func NewStdin() PassphraseReader {
-	return &stdinPassphraseReader{}
+func NewTerminal() PassphraseReader {
+	return &terminalPassphraseReader{}
 }
 
 func NewCaching(upstream PassphraseReader) PassphraseReader {
 	return &cachingPassphraseReader{Upstream: upstream}
 }
 
-type stdinPassphraseReader struct{}
-
-// cachingPassphraseReader will wrap a PassphraseReader by adding caching.
-//
-// This is useful to allow "at most once" semantics when reading the passphrase, while
-// still lazily deferring the first invocation.
-type cachingPassphraseReader struct {
-	Upstream         PassphraseReader
-	cachedPassphrase string
-	cached           bool
+func NewReader(reader io.Reader) PassphraseReader {
+	return &readerPassphraseReader{reader: reader}
 }
 
-func (r *stdinPassphraseReader) ReadPassphrase() (string, error) {
+type terminalPassphraseReader struct{}
+
+func (r *terminalPassphraseReader) ReadPassphrase() (string, error) {
 	if terminal.IsTerminal(0) {
 		_, err := fmt.Fprint(os.Stderr, "Passphrase (saltybox): ")
 		if err != nil {
@@ -58,6 +53,16 @@ func (r *stdinPassphraseReader) ReadPassphrase() (string, error) {
 	return string(data), nil
 }
 
+// cachingPassphraseReader will wrap a PassphraseReader by adding caching.
+//
+// This is useful to allow "at most once" semantics when reading the passphrase, while
+// still lazily deferring the first invocation.
+type cachingPassphraseReader struct {
+	Upstream         PassphraseReader
+	cachedPassphrase string
+	cached           bool
+}
+
 func (r *cachingPassphraseReader) ReadPassphrase() (string, error) {
 	if !r.cached {
 		cached, err := r.Upstream.ReadPassphrase()
@@ -69,4 +74,17 @@ func (r *cachingPassphraseReader) ReadPassphrase() (string, error) {
 	}
 
 	return r.cachedPassphrase, nil
+}
+
+type readerPassphraseReader struct {
+	reader io.Reader
+}
+
+func (r *readerPassphraseReader) ReadPassphrase() (string, error) {
+	data, err := ioutil.ReadAll(r.reader)
+	if err != nil {
+		return "", fmt.Errorf("error reading passphrase: %v", err)
+	}
+
+	return string(data), nil
 }
