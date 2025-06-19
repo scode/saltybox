955b72f82e9f9866b8c69d4c441178474f1e9b8c
Peter Schuller <peter.schuller@infidyne.com>
2025-06-18 18:44:58 -0700

    Add test for Unwrap with invalid base64

--- varmor/varmor_test.go
+++ varmor/varmor_test.go
@@ -64,3 +64,10 @@ func TestAllByteValues(t *testing.T) {
 		"saltybox1:AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0-P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn-AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq-wsbKztLW2t7i5uru8vb6_wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t_g4eLj5OXm5-jp6uvs7e7v8PHy8_T19vf4-fr7_P3-_w",
 		wrapped)
 }
+
+func TestUnwrapBadBase64(t *testing.T) {
+	b, err := Unwrap("saltybox1:bad$$")
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "base64 decoding failed")
+	assert.Nil(t, b)
+}
